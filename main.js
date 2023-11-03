2c1dd95c02ce16397867f5551f60f329372cd9c2
Niels Tack <niels102@live.nl>
2023-11-03 17:00:21 +0100

    template update

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ $(document).ready(function(){
   //End - Don't edit this JS
 
   var tlhidden = new TimelineMax({delay: 0.4})
-      .set('.hiddenonload',{opacity:1})
+    .to('.hiddenOnload',{opacity:1,duration:.3})
+  ;
 
   var tl = new TimelineMax({delay: 0.4})
 
